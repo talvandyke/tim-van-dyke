5f2dbf9f58827857dfce62c8de3c1f3f51819c93
Tim VanDyke <57898188+talvandyke@users.noreply.github.com>
2024-03-25 12:01:23 -0400

    Kinetic-lifts

---
+++ kinetic-lifts/HTML/src/assets/js/app.js
@@ -0,0 +1,170 @@
+/* Template Name: Upwind - Tailwind CSS Landing Page Template
+   Author: Shreethemes
+   Email: support@shreethemes.in
+   Website: https://shreethemes.in
+   Version: 1.7.0
+   Created: March 2022
+   File Description: Main JS file of the template
+*/
+
+
+/*********************************/
+/*         INDEX                 */
+/*================================
+ *     01.  Preloader            *
+ *     02.  Menus                *
+ *     03.  Back to top          *
+ *     04.  Dark & Light Mode    *
+ *     05.  LTR & RTL Mode       *
+ ================================*/
+ window.addEventListener('load', fn, false)
+
+ //  window.onload = function loader() {
+ function fn() {
+     // Preloader
+     if (document.getElementById('preloader')) {
+         setTimeout(() => {
+             document.getElementById('preloader').style.visibility = 'hidden';
+             document.getElementById('preloader').style.opacity = '0';
+         }, 350);
+     }
+ }
+/*********************/
+/*     Menus         */
+/*********************/
+
+function windowScroll() {
+    const navbar = document.getElementById("navbar");
+    if (
+        document.body.scrollTop >= 50 ||
+        document.documentElement.scrollTop >= 50
+    ) {
+        navbar.classList.add("is-sticky");
+    } else {
+        navbar.classList.remove("is-sticky");
+    }
+}
+
+window.addEventListener('scroll', (ev) => {
+    ev.preventDefault();
+    windowScroll();
+})
+
+// Navbar Active Class
+try {
+    var spy = new Gumshoe('#navbar-navlist a', {
+        // Active classes
+        // navClass: 'active', // applied to the nav list item
+        // contentClass: 'active', // applied to the content
+        offset: 80
+    });
+} catch (error) {
+    
+}
+
+
+// Smooth scroll 
+try {
+    var scroll = new SmoothScroll('#navbar-navlist a', {
+        speed: 800,
+        offset: 80
+    });
+} catch (error) {
+    
+}
+
+// Menu Collapse
+const toggleCollapse = (elementId, show = true) => {
+    const collapseEl = document.getElementById(elementId);
+    if (show) {
+        collapseEl.classList.remove('hidden');
+    } else {
+        collapseEl.classList.add('hidden');
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Toggle target elements using [data-collapse]
+    document.querySelectorAll('[data-collapse]').forEach(function (collapseToggleEl) {
+        var collapseId = collapseToggleEl.getAttribute('data-collapse');
+
+        collapseToggleEl.addEventListener('click', function () {
+            toggleCollapse(collapseId, document.getElementById(collapseId).classList.contains('hidden'));
+        });
+    });
+});
+
+window.toggleCollapse = toggleCollapse;
+
+/*********************/
+/*   Back To Top     */
+/*********************/
+
+var mybutton = document.getElementById("back-to-top");
+window.onscroll = function () {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    if(mybutton!=null){
+        if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+            mybutton.style.display = "block";
+        } else {
+            mybutton.style.display = "none";
+        }
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+/*********************/
+/* Dark & Light Mode */
+/*********************/
+try {
+    function changeTheme(e){
+        e.preventDefault()
+        const htmlTag = document.getElementsByTagName("html")[0]
+        
+        if (htmlTag.className.includes("dark")) {
+            htmlTag.className = 'light'
+        } else {
+            htmlTag.className = 'dark'
+        }
+    }
+
+    const switcher = document.getElementById("theme-mode")
+    switcher?.addEventListener("click" ,changeTheme )
+    
+    const chk = document.getElementById('chk');
+
+    chk.addEventListener('change',changeTheme);
+} catch (err) {
+    
+}
+
+
+/*********************/
+/* LTR & RTL Mode */
+/*********************/
+try{
+    const htmlTag = document.getElementsByTagName("html")[0]
+    function changeLayout(e){
+        e.preventDefault()
+        const switcherRtl = document.getElementById("switchRtl")
+        if(switcherRtl.innerText === "LTR"){
+            htmlTag.dir = "ltr"
+        }
+        else{
+            htmlTag.dir = "rtl"
+        }
+        
+    }
+    const switcherRtl = document.getElementById("switchRtl")
+    switcherRtl?.addEventListener("click" ,changeLayout )
+}
+catch(err){}
+
+
